b1b184f1d4c0eaf3a81e76cef4c8dd267a86a5ba
Alex Cacovean <alexandru.cacovean@gmail.com>
2013-07-19 06:23:43 +0000

    revised again web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var startingfile = new Buffer(fs.readFileSync('index.html','utf8'),"utf-8");
+//var startingfile = new Buffer(fs.readFileSync('index.html','utf8'),"utf-8");
 
 
 app.get('/', function(request, response) {
@@ -10,10 +10,8 @@ app.get('/', function(request, response) {
 //  response.send(fs.readFileSync("./index.html").toString());
 
 	response.send('Text works!');
-=======
   //response.send(fs.readFileSync("./index.html").toString());
   response.send("Test");
->>>>>>> eaad9ad233adbc9063d55492f29e9bf616395474
 });
 
 var port = process.env.PORT || 5000;
