72011e895f90823a9ac1ece09daf3f28aeeac1a4
Alex Cacovean <alexandru.cacovean@gmail.com>
2013-07-19 04:17:21 +0000

    updated web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html');//reads index.html content to buffer
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
