dc29e179438784420045b5b1353aad3dd34e7d72
Alex Cacovean <alexandru.cacovean@gmail.com>
2013-07-19 06:02:28 +0000

    changed web.js to read from index.html

--- web.js
+++ web.js
@@ -6,7 +6,9 @@ var startingfile = new Buffer(fs.readFileSync('index.html','utf8'),"utf-8");
 
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("./index.html").toString());
+//  response.send(fs.readFileSync("./index.html").toString());
+
+	response.send("Text works!");
 });
 
 var port = process.env.PORT || 5000;
